093c1d8d96255f1bbe40486e5c6e2ee280946dc1
Ji Luo <ji.luo@nxp.com>
2021-04-01 21:02:07 +0800

    MA-18738-13 Add hwsecure interface
    This commit adds hwsecure interface to be used
    by other TAs to connect to hwsecure.
    
    Change-Id: I0ef7e21f453e2e902cdfd0915084889e6170afd7
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

---
+++ base/lib/hwsecure/include/lib/hwsecure/hwsecure.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <lk/compiler.h>
+#include <stdint.h>
+
+#include <interface/hwsecure/hwsecure.h>
+
+__BEGIN_CDECLS
+
+int set_lcdif_secure_access(int enable);
+
+
+__END_CDECLS
