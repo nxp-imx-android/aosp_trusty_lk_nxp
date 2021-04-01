449437abcf5f6bf62a702988ec7826afbf445eca
Ji Luo <ji.luo@nxp.com>
2021-04-01 21:02:07 +0800

    MA-18738-13 Add hwsecure interface
    This commit adds hwsecure interface to be used
    by other TAs to connect to hwsecure.
    
    Change-Id: I0ef7e21f453e2e902cdfd0915084889e6170afd7
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

---
+++ base/lib/hwsecure/hwsecure.c
@@ -0,0 +1,36 @@
+
+#define TLOG_TAG "secure_fb_ipc"
+
+#include <interface/hwsecure/hwsecure.h>
+
+#include <lib/hwsecure/hwsecure.h>
+#include <lib/tipc/tipc.h>
+#include <uapi/err.h>
+#include <trusty_ipc.h>
+#include <trusty_log.h>
+
+int set_lcdif_secure_access(int enable) {
+    handle_t chan;
+    int rc;
+
+    rc = tipc_connect(&chan, HWSECURE_PORT_NAME);
+    if (rc != NO_ERROR) {
+        TLOGE("failed to connect to TA %s\n", HWSECURE_PORT_NAME);
+        return rc;
+    }
+
+    struct hwsecure_req req;
+    if (enable)
+        req.cmd = HWSECURE_LCDIF_SECURE_ACCESS;
+    else
+        req.cmd = HWSECURE_LCDIF_NON_SECURE_ACCESS;
+
+    rc = tipc_send1(chan, &req, sizeof(req));
+
+    if (rc != (int)(sizeof(req))) {
+        TLOGE("failed to send message and rc=%d\n", rc);
+    }
+
+    close(chan);
+    return rc;
+}
