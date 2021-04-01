01166f9c2625cfd8231847d8beecdd0a6b049cd7
Ji Luo <ji.luo@nxp.com>
2021-04-01 20:21:33 +0800

    MA-18738-11 Add csu driver
    This driver will be registered as syscall to set
    the CSU configuration and trigger secure display.
    
    Test: TUI starts/stop.
    
    Change-Id: Iae8dc4fa168e13ef8db7b451e6b1faa5718a6a53
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

---
+++ platform/imx/common/include/platform/imx_csu.h
@@ -0,0 +1,30 @@
+
+#ifndef _CSU_COMMON_H_
+#define _CSU_COMMON_H_
+
+#define SYSCALL_PLATFORM_FD_CSU 0x8 //Maxium 0xA
+
+#define CSU_OK 0
+#define CSU_ERR 1
+
+#define CSU_IOCMD_STATUS        0x00000001
+#define CSU_IOCMD_CFG_SA        0x00000002
+#define CSU_IOCMD_CFG_CSL       0x00000003
+#define CSU_IOCMD_SECURE_DISP   0x00000004
+
+struct csu_cfg_sa_msg {
+    uint32_t id;
+    uint32_t enable;
+};
+
+struct csu_cfg_csl_msg {
+    uint32_t id;
+    uint32_t val;
+};
+
+struct csu_cfg_secure_disp_msg {
+    uint32_t enable;
+    uint32_t paddr;
+};
+
+#endif
