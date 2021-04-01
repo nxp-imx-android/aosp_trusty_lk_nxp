7cd8799d30f572ebda87eecfd3039c08a3cd4d58
Ji Luo <ji.luo@nxp.com>
2021-04-01 21:01:15 +0800

    MA-18738-12 Add hwsecure TA
    This commit adds hwsecure TA to handle hardware
    related hardware.
    
    Change-Id: I1ae6b7598dfd25b7717eab4baa818d4437b58f52
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

---
+++ app/hwsecure/hwsecure_srv.h
@@ -0,0 +1,10 @@
+#ifndef __HWSECURE_SRV_H__
+#define __HWSECURE_SRV_H__
+
+struct hwservice_context {
+    handle_t *chan;
+};
+
+int add_hwsecure_service(struct tipc_hset *hset, handle_t *chan);
+
+#endif
