a25e93859f04a57aac6bddd93cd04415136533e6
Ji Luo <ji.luo@nxp.com>
2021-04-02 14:24:22 +0800

    MA-18738-14 Enable uuid check
    Check the UUID before some security critical operations
    to restrict TA's access. This commit will allow:
        hwsecure TA    ---> CSU SA/CSL config
        secure fb TA   ---> secure display update
        secure fb TA   ---> hwsecure TA
    
    Test: TUI starts/stop.
    
    Change-Id: I888b4f6d695569267951aba313a7babc0e9cb65f
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- app/hwsecure/hwsecure_srv.cpp
+++ app/hwsecure/hwsecure_srv.cpp
@@ -3,6 +3,7 @@
 #include <trusty_ipc.h>
 #include <trusty_log.h>
 #include <stdlib.h>
+#include <string.h>
 #include <lk/err_ptr.h>
 #include <lib/tipc/tipc_srv.h>
 #include "hwsecure_srv.h"
@@ -12,9 +13,46 @@
 
 static hwservice_context ctx;
 
+struct chan_uuid {
+    handle_t chan;
+    struct uuid peer;
+};
+
+static struct chan_uuid uuid_list[2] = {
+    {
+        .chan = -1,
+        .peer = {0, 0, 0, { 0 }},
+    },
+    {
+        .chan = -1,
+        .peer = {0, 0, 0, { 0 }},
+    },
+};
+
+const static struct uuid secure_fb_impl_ta_uuid = {
+    0x405729b4,
+    0xc12d,
+    0x45d9,
+    {0xae, 0x97, 0x0f, 0x25, 0xaa, 0xa2, 0x04, 0xe4},
+};
+
+static bool check_uuid_equal(const struct uuid* a, const struct uuid* b) {
+    return memcmp(a, b, sizeof(struct uuid)) == 0;
+}
+
+static const struct uuid allowed_uuid[] = {
+                 {
+                     0x405729b4,
+                     0xc12d,
+                     0x45d9,
+                     {0xae, 0x97, 0x0f, 0x25, 0xaa, 0xa2, 0x04, 0xe4},
+                  },
+};
+static const struct uuid *acl_uuid = allowed_uuid;
 static struct tipc_port_acl acl = {
-        .flags = IPC_PORT_ALLOW_TA_CONNECT,
-        // TODO: uuid_num and uuids need set later
+    .flags = IPC_PORT_ALLOW_TA_CONNECT,
+    .uuid_num = 1,
+    .uuids = &acl_uuid,
 };
 
 static struct tipc_port port = {
@@ -29,34 +67,78 @@ static int hwsecure_on_connect(const struct tipc_port* port,
                                 handle_t chan,
                                 const struct uuid* peer,
                                 void** ctx_p) {
+    uint32_t i = 0;
+    struct chan_uuid *ptr = uuid_list;
 
-    TLOGE("hwsecure_on_connect\n");
+    TLOGI("hwsecure_on_connect!\n");
+    for (i = 0; i < sizeof(uuid_list)/sizeof(struct chan_uuid); i++) {
+        if (ptr->chan == -1) {
+            ptr->chan = chan;
+            memcpy(&(ptr->peer), peer, sizeof(struct uuid));
+            return NO_ERROR;
+        }
+        ptr++;
+    }
 
-    return NO_ERROR;
+    TLOGE("Exceed the max hwsecure max channel limit!\n");
+    return ERR_GENERIC;
 }
 
 static void hwsecure_on_disconnect(const struct tipc_port* port,
                                     handle_t chan,
                                     void* ctx) {
+    uint32_t i = 0;
+    struct chan_uuid *ptr = uuid_list;
 
-    TLOGE("hwsecure_on_disconnect\n");
+    TLOGI("hwsecure_on_disconnect!\n");
+    for (i = 0; i < sizeof(uuid_list)/sizeof(struct chan_uuid); i++) {
+        if (ptr->chan == chan) {
+            ptr->chan = -1;
+            memset(&(ptr->peer), 0, sizeof(struct uuid));
+            break;
+        }
+        ptr++;
+    }
+
+    if (i == sizeof(uuid_list)/sizeof(struct chan_uuid))
+        TLOGE("Channel connection info free failed!\n");
 }
 
 static int hwsecure_on_message(const struct tipc_port* port,
                                 handle_t chan,
                                 void* _ctx) {
     int rc;
+    uint32_t list = 0;
     struct hwsecure_req req;
-    rc = tipc_recv1(chan, sizeof(req), &req, sizeof(req));
+    struct chan_uuid *ptr = uuid_list;
 
+    rc = tipc_recv1(chan, sizeof(req), &req, sizeof(req));
     if (rc < 0) {
         TLOGE("failed to recv req \n");
+        return ERR_GENERIC;
+    }
+
+    for (list = 0; list < sizeof(uuid_list)/sizeof(struct chan_uuid); list++) {
+        if (ptr->chan == chan) {
+            break;
+        }
+        ptr++;
+    }
+
+    if (list == sizeof(uuid_list)/sizeof(struct chan_uuid)) {
+        TLOGE("No matching connection find!\n");
+        return ERR_GENERIC;
     }
 
     switch(req.cmd) {
         case HWSECURE_LCDIF_SECURE_ACCESS:
         case HWSECURE_LCDIF_NON_SECURE_ACCESS:
-            set_lcdif_secure(req.cmd);
+            if (check_uuid_equal(&(ptr->peer), &secure_fb_impl_ta_uuid))
+                set_lcdif_secure(req.cmd);
+            else {
+                TLOGE("UUID doesn't match!\n");
+                return ERR_GENERIC;
+            }
             break;
         default:
             return ERR_INVALID_ARGS;
