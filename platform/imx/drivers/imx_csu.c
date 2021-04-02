fd991a88a06da7c4f74c9ab0a13a5fcb6f7bab2f
Ji Luo <ji.luo@nxp.com>
2021-04-02 14:24:22 +0800

    MA-18738-14 Enable uuid check
    Check the UUID before some security critical operations
    to restrict TA's access. This commit will allow:
        hwsecure TA    ---> CSU SA/CSL config
        secure fb TA   ---> secure display update
        secure fb TA   ---> hwsecure TA
    
    Test: TUI starts/stop.
    
    Change-Id: I888b4f6d695569267951aba313a7babc0e9cb65f
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- platform/imx/drivers/imx_csu.c
+++ platform/imx/drivers/imx_csu.c
@@ -14,6 +14,7 @@
 #include <lib/sm/smcall.h>
 #include <lib/sm.h>
 #include <imx-regs.h>
+#include <uapi/trusty_uuid.h>
 
 #define LOCAL_TRACE     5
 
@@ -34,6 +35,24 @@
             (u)->clock_seq_and_node[2], (u)->clock_seq_and_node[3],            \
             (u)->clock_seq_and_node[4], (u)->clock_seq_and_node[5],            \
             (u)->clock_seq_and_node[6], (u)->clock_seq_and_node[7]);
+
+static struct uuid hwsecure_ta_uuid = {
+    0xc52ae02f,
+    0xfa45,
+    0x4d8e,
+    {0x93, 0xe0, 0x6b, 0x51, 0xcf, 0x7a, 0x92, 0x3b},
+};
+
+static struct uuid secure_fb_impl_ta_uuid = {
+    0x405729b4,
+    0xc12d,
+    0x45d9,
+    {0xae, 0x97, 0x0f, 0x25, 0xaa, 0xa2, 0x04, 0xe4},
+};
+
+static bool check_uuid_equal(const struct uuid* a, const struct uuid* b) {
+    return memcmp(a, b, sizeof(struct uuid)) == 0;
+}
 
 static void *csu_base = (void*)CSU_BASE_VIRT;
 
@@ -66,17 +85,25 @@ static int32_t csu_cfg_sa(uint32_t cmd, user_addr_t user_ptr) {
 
 static int32_t sys_csu_ioctl(uint32_t fd, uint32_t cmd, user_addr_t user_ptr) {
     struct trusty_app* app = current_trusty_app();
-    PRINT_TRUSTY_APP_UUID(app->app_id, &app->props.uuid);
     CHECK_FD(fd);
     switch (cmd) {
         case CSU_IOCMD_STATUS:
             return CSU_OK;
         case CSU_IOCMD_CFG_SA:
-            return csu_cfg_sa(cmd, user_ptr);
+            if (check_uuid_equal(&app->props.uuid, &hwsecure_ta_uuid))
+                return csu_cfg_sa(cmd, user_ptr);
+            else
+                return CSU_ERR;
         case CSU_IOCMD_CFG_CSL:
-            return csu_cfg_csl(cmd, user_ptr);
+            if (check_uuid_equal(&app->props.uuid, &hwsecure_ta_uuid))
+                return csu_cfg_csl(cmd, user_ptr);
+            else
+                return CSU_ERR;
         case CSU_IOCMD_SECURE_DISP:
-            return imx_secure_disp(cmd, user_ptr);
+            if (check_uuid_equal(&app->props.uuid, &secure_fb_impl_ta_uuid))
+                return imx_secure_disp(cmd, user_ptr);
+            else
+                return CSU_ERR;
     }
     return CSU_OK;
 }
