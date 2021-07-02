5864b409a8e37dcab80c66e46c22ee5309500b2b
zhai.he <zhai.he@nxp.com>
2021-07-02 15:12:36 +0800

    MA-19233 Enable G2D Secure Pipeline
    Added the interface to allow hwoemcrypto configure the RDC based on
    G2D secure mode and allow to get g2d_secure_mode from linux side.
    
    Change-Id: Ifbfc7f51eee6f5441a0827ee50ae7d412e5dbc76
    Signed-off-by: zhai.he <zhai.he@nxp.com>

--- base/interface/hwsecure/include/interface/hwsecure/hwsecure.h
+++ base/interface/hwsecure/include/interface/hwsecure/hwsecure.h
@@ -11,10 +11,17 @@ enum hwsecure_cmd {
     HWSECURE_LCDIF_SECURE_ACCESS = 2,
     HWSECURE_LCDIF_NON_SECURE_ACCESS = 3,
     HWSECURE_WV_VPU_SECURE = 4,
-    HWSECURE_WV_VPU_NON_SECURE = 5
-
+    HWSECURE_WV_VPU_NON_SECURE = 5,
+    HWSECURE_WV_G2D_SECURE = 6,
+    HWSECURE_WV_G2D_NON_SECURE = 7,
+    HWSECURE_WV_GET_G2D_SECURE_MODE = 8
 };
 
 struct hwsecure_req {
     uint32_t cmd;
+};
+
+enum g2d_secure_mode {
+    SECURE = 1,
+    NON_SECURE = 2
 };
