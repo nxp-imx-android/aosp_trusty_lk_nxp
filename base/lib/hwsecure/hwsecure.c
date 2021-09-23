04d1d865ac4c975e16e9a4e921f60a3d78b3b9fa
zhai.he <zhai.he@nxp.com>
2021-09-23 14:04:14 +0800

    MA-19449 Remove vpu-related interfaces
    Since there is no need to control the RDC permission
    of vpu through tipc, so remove it.
    
    Change-Id: I1656a6d01996c76a8eb08e60ad644e4b4385022a
    Signed-off-by: zhai.he <zhai.he@nxp.com>

--- base/lib/hwsecure/hwsecure.c
+++ base/lib/hwsecure/hwsecure.c
@@ -35,32 +35,6 @@ int set_lcdif_secure_access(int enable) {
     return rc;
 }
 
-int set_widevine_vpu_secure_mode(int secure) {
-    handle_t chan;
-    int rc;
-
-    rc = tipc_connect(&chan, HWSECURE_PORT_NAME);
-    if (rc != NO_ERROR) {
-        TLOGE("failed to connect to TA %s\n", HWSECURE_PORT_NAME);
-        return rc;
-    }
-
-    struct hwsecure_req req;
-    if (secure)
-        req.cmd = HWSECURE_WV_VPU_SECURE;
-    else
-        req.cmd = HWSECURE_WV_VPU_NON_SECURE;
-
-    rc = tipc_send1(chan, &req, sizeof(req));
-
-    if (rc != (int)(sizeof(req))) {
-        TLOGE("failed to send message and rc=%d\n", rc);
-    }
-
-    close(chan);
-    return rc;
-}
-
 int set_widevine_g2d_secure_mode(int secure) {
     handle_t chan;
     int rc;
