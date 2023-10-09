c08fe96213d7040a348f6b3658cfc5930fc37220
Ji Luo <ji.luo@nxp.com>
2023-10-09 16:15:08 +0800

    MA-21701 rework the function to seed boringssl
    we can't override the "CRYPTO_sysrand_for_seed()" anymore after
    a default trusty implementation was added in the "external/boringssl".
    
    However, the default trusty implementation would eventually calls
    "trusty_rng_hw_rand()" which generates hardware backed random without
    the "PR(Prediction Resistance)" set. This commit reworks the function
    as "CRYPTO_sysrand_with_pr()" so it can be called from the trusty
    default implementation of "CRYPTO_sysrand_for_seed()".
    
    Change-Id: I00aff29b5c3a601320490e754f1bf2155e70768c
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- user/lib/nxp_openssl_stub/rand.c
+++ user/lib/nxp_openssl_stub/rand.c
@@ -11,30 +11,18 @@
 #include <uapi/err.h>
 
 #include <lib/rng/trusty_rng.h>
-#include <lib/rng/trusty_rng_internal.h>
 #include <openssl/rand.h>
 #include <platform/imx_rng.h>
 #include <platform/imx_caam.h>
 #include <platform/imx_ele.h>
 
 #if defined(OPENSSL_IS_BORINGSSL)
-/*
- * CRYPTO_sysrand is called by BoringSSL to obtain entropy from the OS on every
- * query for randomness. This needs to be fast, so we provide our own AES-CTR
- * PRNG seeded from hardware randomness, if available.
- */
-void CRYPTO_sysrand(uint8_t* out, size_t requested) {
-    if (trusty_rng_internal_system_rand(out, requested) != NO_ERROR) {
-        abort();
-    }
-}
-
 extern long _trusty_ioctl(uint32_t fd, uint32_t req, void *buf);
 /*
  * seed the BoringSSL RNG from the hardware RNG with
  * "prediction resistance", this shall not fail.
  */
-void CRYPTO_sysrand_for_seed(uint8_t* out, size_t requested) {
+void CRYPTO_sysrand_with_pr(uint8_t* out, size_t requested) {
 #ifdef WITH_CAAM_SUPPORT
     struct pr_rng_msg msg;
 
